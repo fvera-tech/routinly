e49ca00d9bea81fb81fbfed316eb43abfa3674a3
Fabian Vera <fvera.dev@gmail.com>
2026-09-21 18:59:46 -0300

    Definiendo reglas de entrada de datos con type e interface. Se agrega a carpeta assets la imágen del primero bosquejo.

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// Se definen las reglas de los datos a almacenar.
+
+
+type Serie = number | { izquierda: number; derecha: number };
+
+interface Exercise {
+  name: string;
+  series: Serie[]; // .length indica la cantidad de series y su value las repeticiones.
+  weight: number;
+  rest: number;
+}
+
+interface Session {
+  id: number; // Date.now()
+  date: string; // (xx-xx-xxxx)
+  routine: "A" | "B";
+  exercises: Exercise[];
+};
+
